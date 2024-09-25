1c0ee6fd9afb626b4706f4322a586324b9a6aa7c
Sergey <domarch388640@gmail.com>
2024-09-25 16:17:12 +0300

    add refs submit in JS

--- modal.js
+++ modal.js
@@ -1,11 +1,13 @@
 const refs = {
 	openModalBtn: document.querySelector("[data-open-modal]"),
 	closeModalBtn: document.querySelector("[data-close-modal]"),
+	submitModalBtn: document.querySelector("[data-submit-modal]"),
 	backdrop: document.querySelector("[data-backdrop]"),
 };
 
 refs.openModalBtn.addEventListener("click", toggleModal);
 refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.submitModalBtn.addEventListener("click", toggleModal);
 
 refs.backdrop.addEventListener("click", logBackdropClick);
 
